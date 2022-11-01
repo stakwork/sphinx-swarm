b1b9ea0ae3938444e793d960f194bb08df29f6db
Evan Feenstra <evanfeenstra@gmail.com>
2022-11-01 15:07:53 -0700

    fix comment

--- src/cmd/stack/mod.rs
+++ src/cmd/stack/mod.rs
@@ -17,7 +17,7 @@ pub async fn run(docker: Docker) -> Result<()> {
     let btc_id = create_and_start(&docker, btc1).await?;
     log::info!("created bitcoind");
 
-    // btc setup
+    // lnd setup
     let http_port = "38881";
     let lnd_node = images::LndNode::new("lnd1", network, "10009", "/root/.lnd");
     let lnd1 = images::lnd(proj, &lnd_node, &btc_node, Some(http_port));
