fc4ab9a2e754db596644566366e09f1e41b2370c
Evan Feenstra <evanfeenstra@gmail.com>
2022-11-09 12:34:55 -0800

    cleanup stack data over the wire

--- src/modes/stack/handler.rs
+++ src/modes/stack/handler.rs
@@ -1,5 +1,6 @@
 use crate::cmd::{Cmd, RelayCmd, SwarmCmd};
-use crate::config::STACK;
+use crate::config::{Node, Stack, STACK};
+use crate::images::Image;
 use anyhow::Result;
 use bollard::Docker;
 
@@ -11,7 +12,10 @@ pub async fn handle(cmd: Cmd, tag: &str, docker: &Docker) -> Result<String> {
 
     let ret: Option<String> = match cmd {
         Cmd::Swarm(c) => match c {
-            SwarmCmd::GetConfig => Some(serde_json::to_string(&*stack)?),
+            SwarmCmd::GetConfig => {
+                let res = remove_tokens(&*stack);
+                Some(serde_json::to_string(&res)?)
+            }
             SwarmCmd::AddNode(node) => {
                 // add a node via docker
                 None
@@ -28,5 +32,32 @@ pub async fn handle(cmd: Cmd, tag: &str, docker: &Docker) -> Result<String> {
     match ret {
         Some(r) => Ok(r),
         None => Err(anyhow::anyhow!("no return value".to_string())),
+    }
+}
+
+// remove sensitive data from Stack when sending over wire
+fn remove_tokens(s: &Stack) -> Stack {
+    let nodes = s.nodes.iter().map(|n| match n {
+        Node::External(e) => Node::External(e.clone()),
+        Node::Internal(i) => match i.clone() {
+            Image::Btc(mut b) => {
+                b.pass = "".to_string();
+                Node::Internal(Image::Btc(b))
+            }
+            Image::Lnd(mut l) => {
+                l.unlock_password = "".to_string();
+                Node::Internal(Image::Lnd(l))
+            }
+            Image::Proxy(mut p) => {
+                p.store_key = None;
+                p.admin_token = None;
+                Node::Internal(Image::Proxy(p))
+            }
+            Image::Relay(r) => Node::Internal(Image::Relay(r)),
+        },
+    });
+    Stack {
+        network: s.network.clone(),
+        nodes: nodes.collect(),
     }
 }
