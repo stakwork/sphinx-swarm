e4e4cde37076bb33def666c3aca041c686eb6645
elraphty <elraphtyofficial@gmail.com>
2022-11-21 18:21:42 +0100

    created get info function

---
+++ src/conn/bitcoin/bitcoin.rs
@@ -0,0 +1,18 @@
+extern crate bitcoincore_rpc;
+
+use bitcoincore_rpc::{Auth, Client, RpcApi};
+use crate::images::{BtcImage};
+
+pub fn get_info(btc: BtcImage, url: String, port: String) {
+    let btc_url: String = format!("{}:{}", url, port);
+    let rpc = Client::new(
+        &btc_url,
+        Auth::UserPass(
+            btc.user.to_string(),
+            btc.pass.to_string(),
+        ),
+    )
+    .unwrap();
+    let info = rpc.get_blockchain_info().unwrap();
+    println!("Btc Info: {:?}", info);
+}
