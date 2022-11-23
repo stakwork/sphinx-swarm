9dc39c8e7338c4aec57448198afc75b300a58a6f
elraphty <elraphtyofficial@gmail.com>
2022-11-23 23:19:51 +0100

    added thread.sleep for bitcoin rpc call

--- src/conn/bitcoin/bitcoin.rs
+++ src/conn/bitcoin/bitcoin.rs
@@ -1,43 +1,15 @@
 extern crate bitcoincore_rpc;
 
-use std::collections::HashMap;
+use std::{thread, time::Duration};
 
 use crate::images::BtcImage;
 
 use bitcoincore_rpc::{Auth, Client, RpcApi};
 
-use reqwest::Url;
-
-// use bitcoincore_rpc::{Auth, Client, RpcApi};
 pub async fn get_info(btc: &BtcImage, url: String, port: String) {
-    // let btc_url: String = format!("http://{}:{}@{}:{}", btc.user, btc.pass, url, port);
-    let btc_url: String = format!("http://{}:{}", url, port);
-    println!("BTC URL {}",  btc_url);
+    let btc_url: String = format!("{}:{}", url, port);
 
-
-    println!("Useranem {} Pass {}", btc.user, btc.pass);
-
-    // let url = Url::parse(&btc_url).unwrap();
-
-    // println!("BTC URL {} {}",  btc_url, url.has_authority().to_string());
-
-    // let mut map = HashMap::new();
-
-    // map.insert("jsonrpc", "1.0");
-    // map.insert("id", "curltext");
-    // map.insert("method", "getblockchaininfo");
-    // map.insert("params", "[]");
-
-
-    // println!("URL {}", url);
-
-    // let client = reqwest::Client::new();
-    // let res = client
-    //     .post(btc_url)
-    //     .json(&map)
-    //     .send();
-
-    // println!("Result === {:?}", res.await);
+    thread::sleep(Duration::from_millis(40000));
 
     if let Ok(rpc) = Client::new(
         &btc_url,
