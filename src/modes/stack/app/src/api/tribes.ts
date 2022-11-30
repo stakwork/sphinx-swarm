14d2d895589e796e4124bacfbd2f01289219d078
elraphty <elraphtyofficial@gmail.com>
2022-11-30 16:47:42 +0100

    added people

--- src/modes/stack/app/src/api/tribes.ts
+++ src/modes/stack/app/src/api/tribes.ts
@@ -1,18 +1,26 @@
-const IS_DEV = window.location.host === "localhost:8080";
+const IS_DEV = window.location.host === "localhost:13000";
 
-let root = "/tribes";
+let root = "/";
 
 if (IS_DEV) {
-  root = "http://localhost:13000/tribes";
+  root = "http://localhost:13000";
 }
 
 export async function get_tribes(uuid: String = "") {
   let r;
+
   if (!uuid) {
-    r = await fetch(`${root}`);
+    r = await fetch(`${root}/tribe`);
   } else {
-    r = await fetch(`${root}/${uuid}`);
+    r = await fetch(`${root}/tribe/${uuid}`);
   }
+
+  const result = await r.json();
+  return result;
+}
+
+export async function get_people() {
+  const r = await fetch(`${root}/people`);
   const result = await r.json();
   return result;
 }
