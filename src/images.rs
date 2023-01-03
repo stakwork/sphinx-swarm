3f9ed9991ac951a29a634d35b8f92500b490819d
Evan Feenstra <evanfeenstra@gmail.com>
2023-01-03 10:34:42 -0800

    btc image user bitcoin user'

--- src/images.rs
+++ src/images.rs
@@ -301,7 +301,7 @@ pub fn btc(project: &str, node: &BtcImage) -> Config<String> {
     Config {
         image: Some(format!("ruimarinho/bitcoin-core:{}", btc_version)),
         hostname: Some(domain(&node.name)),
-        user: user(),
+        user: Some("bitcoin".to_string()), // from the dockerfile
         cmd: Some(vec![
             format!("-{}=1", node.network),
             format!("-rpcuser={}", node.user),
