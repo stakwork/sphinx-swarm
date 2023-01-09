db3b969e6dd85daf5d0e268f346b4901830ca6c5
elraphty <elraphtyofficial@gmail.com>
2023-01-09 15:42:41 +0100

    continued with proxy api

--- src/conn/proxy/proxyapi.rs
+++ src/conn/proxy/proxyapi.rs
@@ -1,10 +1,14 @@
 use anyhow::Result;
 use serde::{Deserialize, Serialize};
-use std::str::FromStr;
+use std::{str::FromStr, time::Duration};
 
 use crate::images::ProxyImage;
 
-pub struct ProxyAPI;
+pub struct ProxyAPI {
+    pub client: reqwest::Client,
+    pub url: String,
+    pub token: String,
+}
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct ComplexBalance {
@@ -15,12 +19,32 @@ pub struct ComplexBalance {
 }
 
 impl ProxyAPI {
-    pub fn new(proxy: &ProxyImage) -> Result<Self> {
-       // Ok(Self())
-        todo!()
+    pub async fn new(proj: &str, proxy: &ProxyImage) -> Result<Self> {
+        let client = reqwest::Client::builder()
+            .timeout(Duration::from_secs(10))
+            .danger_accept_invalid_certs(true)
+            .build()
+            .expect("couldnt build reqwest client for proxy");
+            
+        Ok(Self {
+            url: format!("localhost:{}", proxy.port),
+            client,
+            token: proxy.admin_token.clone().unwrap_or("".to_string()),
+        })
     }
 
     pub async fn get_balance(&self) -> Result<ComplexBalance> {
-        todo!();
+        let route = format!("http://{}/balance", self.url);
+        match self
+            .client
+            .get(route.as_str())
+            .header("Content-Type", "application/json")
+            .header("x-user-token", self.token.clone())
+            .send()
+            .await
+        {
+            Ok(res) => Ok(res.json().await?),
+            Err(e) => Err(anyhow::anyhow!("Proxy Balance Error {:?}", e)),
+        }
     }
 }
