24eb2e7c378ba4c1f33a2bc8a111c8c7b3bf1944
elraphty <elraphtyofficial@gmail.com>
2023-02-16 23:47:11 +0100

    changed action buttons to icon-only buttons

--- src/bin/stack/app/src/nodes/NodeAction.svelte
+++ src/bin/stack/app/src/nodes/NodeAction.svelte
@@ -1,4 +1,7 @@
 <script lang="ts">
+  import { Button, InlineLoading } from "carbon-components-svelte";
+  import Play from "carbon-icons-svelte/lib/Play.svelte";
+  import Stop from "carbon-icons-svelte/lib/Power.svelte";
   import * as api from "../api";
   import { selectedNode, containers, node_state } from "../store";
   import type { Container } from "../api/swarm";
@@ -28,56 +31,34 @@
     listContainers();
     btnDis = false;
   }
-
-  $: stateClass =
-    $node_state === "running"
-      ? "btn-stop"
-      : $node_state === "restarting"
-      ? "btn-restart"
-      : "btn-start";
 </script>
 
 <aside class="node-action-wrap">
-  <button
-    disabled={btnDis}
-    on:click={() =>
-      $node_state === "running"
-        ? stopContainer(`${$selectedNode.name}.sphinx`)
-        : startContainer(`${$selectedNode.name}.sphinx`)}
-    class={`btn ${stateClass}`}
-  >
-    {#if $node_state === "running"}
-      Stop
-    {:else if $node_state === "restarting"}
-      Restarting
-    {:else}
-      Start
-    {/if}
-  </button>
+  {#if $node_state === "running"}
+    <Button
+      disabled={btnDis}
+      class="btn-stop"
+      on:click={() => stopContainer(`${$selectedNode.name}.sphinx`)}
+      iconDescription={`Stop ${$selectedNode.name}`}
+      icon={Stop}
+      size="small"
+    />
+  {:else if $node_state === "restarting"}
+    <InlineLoading description={`Restarting ${$selectedNode.name}...`} />
+  {:else}
+    <Button
+      disabled={btnDis}
+      class="btn-start"
+      on:click={() => startContainer(`${$selectedNode.name}.sphinx`)}
+      iconDescription={`Start ${$selectedNode.name}`}
+      icon={Play}
+      size="small"
+    />
+  {/if}
 </aside>
 
 <style>
   .node-action-wrap {
     margin-left: 20px;
-  }
-  .node-action-wrap .btn {
-    padding: 6px 8px;
-    border: 0px;
-    outline: 0px;
-    border-radius: 2px;
-    color: #fff;
-    font-size: 0.75rem;
-    font-weight: 600;
-    cursor: pointer;
-  }
-
-  .node-action-wrap .btn-stop {
-    background-color: red;
-  }
-  .node-action-wrap .btn-start {
-    background-color: green;
-  }
-  .node-action-wrap .btn-restart {
-    background-color: gold;
   }
 </style>
