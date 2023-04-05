cce99d6aa6f5443c93a3b26df5f9fcf4bad9c672
Oluwatobi Bamidele <teebams49@gmail.com>
2023-04-05 22:14:21 +0100

    update: added qrToken to config file

--- testscripts/test-signup.js
+++ testscripts/test-signup.js
@@ -1,17 +1,22 @@
 import fetch from "node-fetch";
+import { configuration } from "./config/config.js";
 
 async function testSignup() {
-  const qrToken = `claim::localhost:3000::MDZ2Tm9zVlBBNTND`;
-  const arr = qrToken.split("::");
-  const ip = arr[1];
-  const token = decodeToken(arr[2]);
-  const contacts = await getContact(token, ip);
-  const contact = contacts.response.contacts[0];
-  const updatedContact = await updateContact(ip, token, contact.id);
-  if (updatedContact.success) {
-    console.log("====> admin setup successfully <======");
-  } else {
-    console.log("=====> An error occured <======");
+  try {
+    const qrToken = configuration.qrToken;
+    const arr = qrToken.split("::");
+    const ip = arr[1];
+    const token = decodeToken(arr[2]);
+    const contacts = await getContact(token, ip);
+    const contact = contacts.response.contacts[0];
+    const updatedContact = await updateContact(ip, token, contact.id);
+    if (updatedContact.success) {
+      console.log("====> admin setup successfully <======");
+    } else {
+      console.log("=====> An error occured <======");
+    }
+  } catch (error) {
+    console.log(error);
   }
 }
 
