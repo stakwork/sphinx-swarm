804200d4802666031042a1ac328dca23a6358ae1
Oluwatobi Bamidele <teebams49@gmail.com>
2023-04-05 20:19:57 +0100

    feat: testscript for signup

---
+++ testscripts/test-signup.js
@@ -0,0 +1,48 @@
+import fetch from "node-fetch";
+
+async function testSignup() {
+  const qrToken = `claim::localhost:3000::ZHE1bnJtcnVBdmtT`;
+  const arr = qrToken.split("::");
+  const ip = arr[1];
+  const token = decodeToken(arr[2]);
+  const contacts = await getContact(token, ip);
+  const contact = contacts.response.contacts[0];
+  const updatedContact = await updateContact(ip, token, contact.id);
+  if (updatedContact.success) {
+    console.log("====> admin setup successfully <======");
+  } else {
+    console.log("=====> An error occured <======");
+  }
+}
+
+function decodeToken(token) {
+  return Buffer.from(token, "base64").toString("utf8");
+}
+
+async function getContact(token, ip) {
+  try {
+    const res = await fetch(`http://${ip}/contacts`, {
+      method: "GET",
+      headers: { "x-user-token": token, "Content-Type": "application/json" },
+    });
+
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function updateContact(ip, token, id) {
+  try {
+    const res = await fetch(`http://${ip}/contacts/${id}`, {
+      method: "PUT",
+      headers: { "x-user-token": token, "Content-Type": "application/json" },
+      body: JSON.stringify({ alias: "admin_user" }),
+    });
+    return await res.json();
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+await testSignup();
