a3d8f24cb99a6e0fb023d1c996327ad9e99da720
elraphty <elraphtyofficial@gmail.com>
2023-06-06 16:19:36 +0100

    config

---
+++ src/bin/stack/app/node_modules/svelte-dialogs/src/components/Confirm.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { default as DialogContent } from "./DialogContent.svelte";
+  import { getClose, getOptions } from "../lib/ctx-manager";
+
+  /** retrieve close function from context */
+  const close = getClose();
+
+  /** retrieve options from context */
+  const { declineButtonClass, declineButtonText, confirmButtonClass, confirmButtonText } =
+    getOptions();
+</script>
+
+<!-- 
+  @component
+  Default content of confirm().
+  Adds decline and confirm buttons in DialogContent footer slot
+  that call close function with false and true.
+ -->
+<DialogContent>
+  <svelte:fragment slot="footer">
+    <button
+      class={declineButtonClass}
+      aria-label="Decline"
+      data-testid="confirm__decline-button"
+      on:click={() => close(false)}>{@html declineButtonText}</button
+    >
+    <button
+      class={confirmButtonClass}
+      aria-label="Confirm"
+      data-testid="confirm__confirm-button"
+      on:click={() => close(true)}>{@html confirmButtonText}</button
+    >
+  </svelte:fragment>
+</DialogContent>
