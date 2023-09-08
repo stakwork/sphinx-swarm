e35b0c66c2b0247111cb891dc55b056828f56821
Oluwatobi Bamidele <teebams49@gmail.com>
2023-09-08 15:16:51 +0100

    feat: cln api

---
+++ src/conn/cln/api.rs
@@ -0,0 +1,45 @@
+use crate::images::cln::ClnImage;
+use crate::utils::docker_domain;
+use anyhow::Result;
+use serde::{Deserialize, Serialize};
+use std::{collections::HashMap, time::Duration};
+
+pub struct ClnAPI {
+    pub client: reqwest::Client,
+    pub url: String,
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Clients {
+    total: u128,
+    balances: HashMap<String, u32>,
+}
+
+impl ClnAPI {
+    pub async fn new(cln: &ClnImage) -> Result<Self> {
+        let client = reqwest::Client::builder()
+            .timeout(Duration::from_secs(20))
+            .danger_accept_invalid_certs(true)
+            .build()
+            .expect("couldnt build proxy reqwest client");
+        let host = docker_domain(&cln.name);
+        Ok(Self {
+            url: format!("{}:8010", &host),
+            client
+        })
+    }
+
+    pub async fn get_clients(&self) -> Result<> {
+        let route = format!("http://{}/clients", self.url);
+
+        let res = self
+            .client
+            .get(route.as_str())
+            .send()
+            .await?;
+
+        let clients = res.json().await?;
+        println!("{:?}",clients)
+        Ok()
+    }
+}
