e54a86fd5313803ee737c179a124ed8d427f0e93
Evan Feenstra <evanfeenstra@gmail.com>
2024-01-22 08:44:31 -0800

    tribe v2

--- scripts/restart_sphinx.sh
+++ scripts/restart_sphinx.sh
@@ -1,6 +1,7 @@
 docker pull sphinxlightning/sphinx-swarm:latest
 docker pull sphinxlightning/sphinx-broker:latest
 docker pull sphinxlightning/sphinx-mixer:latest
+docker pull sphinxlightning/sphinx-tribes-v2:latest
 
 docker stop swarm.sphinx && docker rm swarm.sphinx
 
