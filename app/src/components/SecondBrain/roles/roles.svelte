1a95d1189c27a46f7ea1e269f8e98e929bfd2564
Oluwatobi Bamidele <teebams49@gmail.com>
2024-03-13 23:19:52 +0100

    update: get super admin on page mount

--- app/src/components/SecondBrain/roles/roles.svelte
+++ app/src/components/SecondBrain/roles/roles.svelte
@@ -1,6 +1,23 @@
 <script lang="ts">
   import SetSuperAdmin from "./setSuperAdmin.svelte";
   import { boltwallSuperAdminPubkey } from "../../../store";
+  import { onMount } from "svelte";
+  import { get_super_admin } from "../../../api/swarm";
+
+  async function checkSuperAdminExist() {
+    const result = await get_super_admin();
+    const parsedResult = JSON.parse(result);
+    if (
+      parsedResult?.success &&
+      parsedResult.message === "super admin record"
+    ) {
+      boltwallSuperAdminPubkey.set(parsedResult.data.pubkey);
+    }
+  }
+
+  onMount(async () => {
+    checkSuperAdminExist();
+  });
 </script>
 
 <div class="container">
