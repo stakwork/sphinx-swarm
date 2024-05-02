bb5819d89017971935b5ef2cdee42d42115f3446
Oluwatobi Bamidele <teebams49@gmail.com>
2024-05-02 20:06:40 +0100

    update: updated endpoint permission UI

--- app/src/components/EnpointPermission.svelte
+++ app/src/components/EnpointPermission.svelte
@@ -4,6 +4,7 @@
   import Endpoint from "./Endpoint.svelte";
 
   $: allEndpoints = [];
+  $: success = false;
 
   async function getAllPaidEndpoint() {
     const endpoints = await list_all_paid_endpoint();
@@ -13,14 +14,32 @@
     }
   }
 
+  function handleCustomEvent(event) {
+    success = event.detail;
+
+    setTimeout(() => {
+      success = false;
+    }, 5000);
+  }
+
   onMount(async () => {
     await getAllPaidEndpoint();
   });
 </script>
 
 <div class="endpoint-permission-container">
+  <div class="endpoint-header-container">
+    <h2 class="endpoint_header">Payments</h2>
+    {#if success}
+      <div class="success_container">
+        <img src="swarm/check_circle.svg" alt="success" />
+        <p class="success_text">Endpoint Updated</p>
+      </div>
+    {/if}
+  </div>
   {#each allEndpoints as endpoint, index (endpoint.id)}
     <Endpoint
+      on:customEvent={handleCustomEvent}
       description={endpoint.route_description}
       toggled={endpoint.status}
       id={endpoint.id}
@@ -33,5 +52,35 @@
     padding-top: 1.5rem;
     padding-left: 2.25rem;
     padding-right: 2.25rem;
+  }
+
+  .endpoint-header-container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 1.2rem 0rem;
+  }
+
+  .endpoint_header {
+    font-family: "Barlow";
+    font-size: 1.125rem;
+    font-weight: 700;
+    line-height: 1rem;
+    letter-spacing: 0.01em;
+  }
+
+  .success_container {
+    display: flex;
+    align-items: center;
+  }
+
+  .success_text {
+    font-family: "Roboto";
+    font-size: 0.8125rem;
+    font-weight: 400;
+    line-height: 1rem;
+    letter-spacing: 0.01em;
+    color: #49c998;
+    margin-left: 0.75rem;
   }
 </style>
