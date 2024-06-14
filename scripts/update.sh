87ec0de509da3c14acf9f7ecbe1f60d7e06bec25
Evan Feenstra <evanfeenstra@gmail.com>
2024-06-14 12:11:34 -0700

    fix update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,7 +1,17 @@
+echo "pull images"
+docker pull sphinxlightning/sphinx-broker:latest
+docker pull sphinxlightning/sphinx-mixer:latest
+docker pull sphinxlightning/sphinx-swarm:latest
+docker pull sphinxlightning/sphinx-tribes-v2:latest
+
+echo "stop swarm.sphinx"
 docker stop swarm.sphinx
 docker rm swarm.sphinx
-./pull.sh
+
+echo "stop $1.sphinx"
 docker stop $1.sphinx
 docker rm $1.sphinx
+
+echo "start sphinxv2"
 docker-compose -f sphinxv2.yml up swarm.sphinx -d
 docker logs swarm.sphinx --follow
