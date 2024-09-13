b15f989e212605692975a04035dde0b64b1f6929
Oluwatobi Bamidele <teebams49@gmail.com>
2024-09-13 20:39:11 +0100

    feat: add domain name to route33

--- src/bin/super/util.rs
+++ src/bin/super/util.rs
@@ -5,6 +5,10 @@ use aws_config::meta::region::RegionProviderChain;
 use aws_config::Region;
 use aws_sdk_ec2::types::{BlockDeviceMapping, EbsBlockDevice, InstanceType, Tag, TagSpecification};
 use aws_sdk_ec2::Client;
+use aws_sdk_route53::types::{
+    Change, ChangeAction, ChangeBatch, ResourceRecord, ResourceRecordSet,
+};
+use aws_sdk_route53::Client as Route53Client;
 use aws_smithy_types::retry::RetryConfig;
 use futures_util::TryFutureExt;
 use reqwest::Response;
@@ -253,7 +257,7 @@ async fn create_ec2_instance() -> Result<String, Error> {
 
     let super_token = getenv("SUPER_TOKEN")?;
 
-    let swarm_name = "swarm46";
+    let swarm_name = "swarm48";
 
     // Load the AWS configuration
     let config = aws_config::from_env()
@@ -398,10 +402,59 @@ async fn get_instance_ip(instance_id: &str) -> Result<String, Error> {
     Ok(public_ip_address.to_string())
 }
 
+async fn add_domain_name_to_route53(domain_name: &str, public_ip: &str) -> Result<(), Error> {
+    let region = getenv("AWS_S3_REGION_NAME")?;
+    let hosted_zone_id = getenv("ROUTE53_ZONE_ID")?;
+    let region_provider = RegionProviderChain::first_try(Some(Region::new(region)));
+    let config = aws_config::from_env()
+        .region(region_provider)
+        .retry_config(RetryConfig::standard().with_max_attempts(10))
+        .load()
+        .await;
+    let route53_client = Route53Client::new(&config);
+
+    let resource_record = ResourceRecord::builder().value(public_ip).build()?;
+
+    let resource_record_set = ResourceRecordSet::builder()
+        .name(domain_name)
+        .r#type("A".into()) // A record for IPv4
+        .ttl(300) // Time-to-live (in seconds)
+        .resource_records(resource_record)
+        .build()
+        .map_err(|err| anyhow!(err.to_string()))?;
+
+    // Create a change request to upsert (create or update) the A record
+    let change = Change::builder()
+        .action(ChangeAction::Upsert)
+        .resource_record_set(resource_record_set)
+        .build()
+        .map_err(|err| anyhow!(err.to_string()))?;
+
+    let change_batch = ChangeBatch::builder()
+        .changes(change)
+        .build()
+        .map_err(|err| anyhow!(err.to_string()))?;
+
+    let response = route53_client
+        .change_resource_record_sets()
+        .hosted_zone_id(hosted_zone_id)
+        .change_batch(change_batch)
+        .send()
+        .await?;
+
+    log::info!(
+        "Route 53 change status for {}: {:?}",
+        domain_name,
+        response.change_info()
+    );
+
+    Ok(())
+}
+
 pub async fn create_swarm_ec2() -> Result<(), Error> {
-    log::info!("About to get into the creating ec2 instance");
     let ec2_intance_id = create_ec2_instance().await?;
     let ec2_ip_address = get_instance_ip(&ec2_intance_id).await?;
-    log::info!("{}", ec2_ip_address);
+    let _ = add_domain_name_to_route53("*.swarm48.sphinx.chat", &ec2_ip_address).await?;
+    log::info!("Public_IP: {}", ec2_ip_address);
     Ok(())
 }
