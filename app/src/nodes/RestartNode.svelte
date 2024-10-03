0a33b98135ba45448a03136ecc3102f5b3c7486d
Oluwatobi Bamidele <teebams49@gmail.com>
2024-10-03 17:57:56 +0100

    feat: create rstart button

---
+++ app/src/nodes/RestartNode.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+  import { Button, InlineLoading } from "carbon-components-svelte";
+  import { selectedNode } from "../store";
+  import { Upgrade, Restart } from "carbon-icons-svelte";
+
+  function restartContainer() {}
+</script>
+
+<aside>
+  {#if updating}
+    <InlineLoading description={`Restarting ${$selectedNode.name}...`} />
+  {:else}
+    <Button
+      kind="primary"
+      disabled={updating}
+      class="btn-stop"
+      on:click={restartContainer}
+      iconDescription={`Upgrade ${$selectedNode.name}`}
+      icon={Restart}
+      size="field"
+    />
+  {/if}
+</aside>
+
+<style></style>
