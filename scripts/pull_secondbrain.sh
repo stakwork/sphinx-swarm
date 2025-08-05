1a77b9aaea45ff7de60f32e2639dd61779017e41
Oluwatobi Bamidele <teebams49@gmail.com>
2025-08-05 13:32:51 +0100

    chore: settle image

--- scripts/pull_secondbrain.sh
+++ scripts/pull_secondbrain.sh
@@ -1,10 +1,10 @@
 docker image prune -af
-docker pull sphinxlightning/sphinx-swarm:latest
-docker pull sphinxlightning/sphinx-boltwall:latest
+docker pull sphinxlightning/sphinx-swarm:test-arm64-5bf7f2d
+docker pull sphinxlightning/sphinx-boltwall:v0.0.1.181
 # docker pull sphinxlightning/sphinx-jarvis-backend:latest
-docker pull sphinxlightning/sphinx-nav-fiber:latest
-docker pull sphinxlightning/sphinx-runner:latest
+docker pull sphinxlightning/sphinx-nav-fiber:v0.1.842
+# docker pull sphinxlightning/sphinx-runner:latest
 docker pull ghcr.io/stakwork/stakgraph-mcp:latest
 # docker pull ghcr.io/evanfeenstra/chrome-playwright:latest
 docker pull ghcr.io/stakwork/stakgraph-standalone:latest
-docker pull elasticsearch:8.11.1
+# docker pull elasticsearch:8.11.1
