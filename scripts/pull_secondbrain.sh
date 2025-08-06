a837e0eabb30a33cd2bf5782a16b20a7a388f325
Oluwatobi Bamidele <teebams49@gmail.com>
2025-08-06 03:52:17 +0100

    chore: use arm64 images

--- scripts/pull_secondbrain.sh
+++ scripts/pull_secondbrain.sh
@@ -1,8 +1,8 @@
 docker image prune -af
-docker pull sphinxlightning/sphinx-swarm:test-arm64-5bf7f2d
-docker pull sphinxlightning/sphinx-boltwall:v0.0.1.181
+docker pull sphinxlightning/sphinx-swarm:test-arm64-6c63fb2
+docker pull sphinxlightning/sphinx-boltwall:test-arm64-v0.0.1.182
 # docker pull sphinxlightning/sphinx-jarvis-backend:latest
-docker pull sphinxlightning/sphinx-nav-fiber:v0.1.842
+docker pull sphinxlightning/sphinx-nav-fiber:test-arm64-v0.1.844
 # docker pull sphinxlightning/sphinx-runner:latest
 docker pull ghcr.io/stakwork/stakgraph-mcp:latest
 # docker pull ghcr.io/evanfeenstra/chrome-playwright:latest
