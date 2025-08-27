702b9999fa9f7d91cf3deddc2075212c9f628c3c
Oluwatobi Bamidele <teebams49@gmail.com>
2025-08-27 17:32:28 +0100

    chore: get super admin logs backend

---
+++ src/bin/super/service/super_admin_logs.rs
@@ -0,0 +1,24 @@
+use sphinx_swarm::dock::{container_logs, dockr};
+
+use crate::cmd::SuperSwarmResponse;
+
+pub async fn get_super_admin_docker_logs() -> SuperSwarmResponse {
+    let docker = dockr();
+    let logs = container_logs(&docker, "redis_mava_pay").await;
+    let json_value = match serde_json::to_value(logs) {
+        Ok(data) => data,
+        Err(err) => {
+            return SuperSwarmResponse {
+                success: false,
+                message: err.to_string(),
+                data: None,
+            };
+        }
+    };
+
+    return SuperSwarmResponse {
+        success: true,
+        message: "Super admin docker logs".to_string(),
+        data: Some(json_value),
+    };
+}
