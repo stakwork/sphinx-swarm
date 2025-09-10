fe7abbf764fb69ef9f307742d63c126b2929e144
Oluwatobi Bamidele <teebams49@gmail.com>
2025-09-10 12:30:12 +0100

    chore: ensure reserved swarms are stored immediately they are created

--- src/bin/super/service/swarm_reserver/reserve_swarm.rs
+++ src/bin/super/service/swarm_reserver/reserve_swarm.rs
@@ -4,7 +4,7 @@ use tokio::time::{sleep, Duration};
 use crate::{
     route53::add_domain_name_to_route53,
     service::swarm_reserver::utils::generate_random_secret,
-    state::{self, default_reserved_instances, AvailableInstances, ReservedInstances},
+    state::{self, default_reserved_instances, AvailableInstances},
     util::{create_ec2_instance, get_instance_ip},
 };
 pub async fn handle_reserve_swarms() -> Result<()> {
@@ -29,7 +29,6 @@ pub async fn handle_reserve_swarms() -> Result<()> {
     }
 
     log::info!("Reserving {} instances", amount_to_reserve);
-    let mut new_reserved_instances: Vec<AvailableInstances> = Vec::new();
 
     for _ in 0..amount_to_reserve {
         // TODO: decide what we want to be the default instance type: ASK GONZALO
@@ -56,45 +55,27 @@ pub async fn handle_reserve_swarms() -> Result<()> {
 
         let _ = add_domain_name_to_route53(domain_names.clone(), &ec2_ip_address).await?;
 
-        new_reserved_instances.push(AvailableInstances {
-            instance_id: ec2_instance.0,
-            instance_type: instance_type.to_string(),
-            swarm_number: ec2_instance.1.to_string(),
-            default_host: format!("swarm{}.sphinx.chat:8800", ec2_instance.1),
-            user: None,
-            pass: None,
-            ip_address: Some(ec2_ip_address),
-            host,
-            admin_password,
-        });
+        let mut state = state::STATE.lock().await;
+
+        state
+            .reserved_instances
+            .as_mut()
+            .unwrap()
+            .available_instances
+            .push(AvailableInstances {
+                instance_id: ec2_instance.0,
+                instance_type: instance_type.to_string(),
+                swarm_number: ec2_instance.1.to_string(),
+                default_host: format!("swarm{}.sphinx.chat:8800", ec2_instance.1),
+                user: None,
+                pass: None,
+                ip_address: Some(ec2_ip_address),
+                host,
+                admin_password,
+            });
+
+        drop(state)
     }
-
-    let mut state = state::STATE.lock().await;
-
-    if new_reserved_instances.len() == 0 {
-        return Ok(());
-    }
-
-    let mut merged_available_instance: Vec<AvailableInstances> = Vec::new();
-
-    let mut reserved_instances = state.reserved_instances.clone();
-
-    let reserved_available_instances = reserved_instances.clone().unwrap().available_instances;
-
-    for available_instance in reserved_available_instances {
-        merged_available_instance.push(available_instance);
-    }
-
-    for available_instance in new_reserved_instances {
-        merged_available_instance.push(available_instance);
-    }
-
-    reserved_instances = Some(ReservedInstances {
-        minimum_available: reserved_instances.clone().unwrap().minimum_available,
-        available_instances: merged_available_instance.clone(),
-    });
-
-    state.reserved_instances = reserved_instances;
 
     Ok(())
 }
